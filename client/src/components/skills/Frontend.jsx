a825cc703f8e7f0b33387a3f1aac09b85c85be8c
Jinumenan <jinumenasarma@gmail.com>
2024-12-24 16:45:23 +0530

    demo cod

---
+++ client/src/components/skills/Frontend.jsx
@@ -0,0 +1,74 @@
+import React from 'react'
+
+
+const Frontend = () => {
+  return (
+    <>
+        <div className="skills__content">
+            <h3 className="skills__title">Frontend Developer</h3>
+            <div className="skills__box">
+                <div className="flex flex-col">
+                        <div className="skills__group">
+                            <div className="skills__data">
+                                <i className="bx bx-badge-check"></i>
+
+                                <div>
+                                    <h3 className="skills__name">Python</h3>
+                                    <span className="skills__level">Basic</span>
+                                </div>
+                            </div>
+
+                            <div className="skills__data">
+                                <i className="bx bx-badge-check"></i>
+
+                                <div>
+                                    <h3 className="skills__name">Power BI</h3>
+                                    <span className="skills__level">Advanced</span>
+                                </div>
+                            </div>
+
+                            <div className="skills__data">
+                                <i className="bx bx-badge-check"></i>
+
+                                <div>
+                                    <h3 className="skills__name">SQL</h3>
+                                    <span className="skills__level">Intermediate</span>
+                                </div>
+                            </div>
+
+                            <div className="skills__data">
+                                <i className="bx bx-badge-check"></i>
+
+                                <div>
+                                    <h3 className="skills__name">R Studio</h3>
+                                    <span className="skills__level">Advanced</span>
+                                </div>
+                            </div>
+
+                            <div className="skills__data">
+                                <i className="bx bx-badge-check"></i>
+
+                                <div>
+                                    <h3 className="skills__name">Excel</h3>
+                                    <span className="skills__level">Intermediate</span>
+                                </div>
+                            </div>
+
+                            <div className="skills__data">
+                                <i className="bx bx-badge-check"></i>
+
+                                <div>
+                                    <h3 className="skills__name">Git</h3>
+                                    <span className="skills__level">Intermediate</span>
+                                </div>
+                            </div>
+                        </div>
+
+                </div>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default Frontend
