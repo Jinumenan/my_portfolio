3e3ece2191497efabfd3ad0d53c038f25b425d30
Jinumenan <jinumenasarma@gmail.com>
2025-08-06 10:36:35 +0530

    update Resume

--- docs/src/components/about/About.jsx
+++ docs/src/components/about/About.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import "./about.css"
 import me3 from "../../assets/me3.jpeg";
 import Info from './Info';
-import CV from "../../assets/John-Cv.pdf"
+import CV from "../../assets/jinu.pdf"
 
 export const About = () => {
   return (
